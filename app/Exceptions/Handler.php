53b0afbf597b7e211a1f88ec423ef220980bec13
Syed Sirajul Islam Anik <sirajul.islam.anik@gmail.com>
2017-11-07 04:07:41 +0600

    NotFoundHttpExceptionHandled from Handler.php

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -6,6 +6,7 @@ use Illuminate\Auth\AuthenticationException;
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
 use Illuminate\Validation\UnauthorizedException;
 use Illuminate\Validation\ValidationException;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
 class Handler extends ExceptionHandler
 {
@@ -28,6 +29,10 @@ class Handler extends ExceptionHandler
 		$statusCode = 400;
 		$data = [];
 		switch (get_class($exception)) {
+			case NotFoundHttpException::class:
+				$statusCode = 404;
+				$data = [ 'route' => 'Invalid URL.' ];
+				break;
 			case ValidationException::class:
 				$statusCode = 422;
 				foreach ($exception->errors() as $key => $value) {
